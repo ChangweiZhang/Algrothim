c8ac616f8d752a3f136a9bfd252c1767e842c09a
changwei <z18205051832@hotmail.com>
2016-08-12 22:49:26 +0800

    add source

---
+++ LeetCode/TwoSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Solution();
+            var testcase = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var result = program.TwoSum(testcase, target);
+
+        }
+
+
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var val = nums[i];
+                if (dict.ContainsKey(val))
+                {
+                    return new int[] { dict[val], i };
+                }
+
+                dict[target - val] = i;
+
+            }
+            return null;
+        }
+    }
+}
